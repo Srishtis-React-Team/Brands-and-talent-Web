9a4050b88855410a9577fd5331f58fd4e44345fd
ratheesh-srishtis <ratheeshsam.sics@gmail.com>
2024-01-12 17:15:18 +0530

    pricing-design

--- src/views/pricing.js
+++ src/views/pricing.js
@@ -42,153 +42,76 @@ const Pricing = () => {
           </div>
         </div>
       </section>
-      <div className="pricing-section">
-        <div className="guarantee-wrapper">
-          <div>
-            <img src={checkShield}></img>
+      <div className="plans-section">
+        <div className="plans-wrapper free-plans">
+          <div className="premium-text">Premium</div>
+          <div className="plan-value">Free</div>
+          <div className="plan-validity">Forever</div>
+          <div className="choose-btn free-btn">Choose plan</div>
+          <div className="include">WHAT'S INCLUDED</div>
+          <div className="included-things">
+            <div>1. Create a profile </div>
+            <div>2. Verified profile</div>
+            <div>3. portfolio</div>
+            <div>4. 3 photos</div>
+            <div>5. 1 videos</div>
+            <div>6. Bio</div>
+            <div>7. CV</div>
+            <div>8. Get Invited to a Job</div>
+            <div>9. Apply to unlimited jobs</div>
+            <div>10. Chat with thebrands (clients)directly</div>
           </div>
-          <div className="plan-name">Our Top</div>
-          <div className="plan-name-bold">Plans</div>
+          <div className="learn-btn">Learn More</div>
         </div>
-        <div
-          className={plan1_Selected ? "pricing-fill" : "pricing-wrapper"}
-          onClick={(e) => {
-            handleForms("plan-1");
-          }}
-        >
-          <div className="plan-name">Starter</div>
-          <div className="plan-value">$0</div>
-          <div className="plan-duration">per month</div>
-          <div className="plan-action center">Choose plan</div>
+        <div className="plans-wrapper pro-plans">
+          <div className="plan-name">
+            Pro (Recommended)
+            <div className="pro-gift">Gift Subscription</div>
+          </div>
+          <div className="plan-value">$7 per / month</div>
+          <div className="save-plan">
+            Annual (save 22.2%) US$ 7/month <span>US$ 108</span>  US $84/year
+          </div>
+          <div className="choose-btn pro-btn">Choose plan</div>
+          <div className="include">WHAT'S INCLUDED</div>
+          <div className="included-things">
+            <div>1. Create a profile </div>
+            <div>2. Verified profile</div>
+            <div>3. portfolio</div>
+            <div>4. 20 photos</div>
+            <div>5. 6 videos</div>
+            <div>6. Bio</div>
+            <div>7. CV</div>
+            <div>8. Get Invited to a Job</div>
+            <div>9. Apply to unlimited jobs</div>
+            <div>10. Chat with thebrands (clients)directly</div>
+          </div>
+          <div className="learn-btn">Learn More</div>
         </div>
-        <div
-          className={plan2_Selected ? "pricing-fill" : "pricing-wrapper"}
-          onClick={(e) => {
-            handleForms("plan-2");
-          }}
-        >
-          <div className="plan-name">Enterprise</div>
-          <div className="plan-value">$19</div>
-          <div className="plan-duration">per month</div>
-
-          <div className="plan-action center">Choose plan</div>
-        </div>
-        <div
-          className={plan3_Selected ? "pricing-fill" : "pricing-wrapper"}
-          onClick={(e) => {
-            handleForms("plan-3");
-          }}
-        >
-          <div className="plan-name">Premium</div>
-          <div className="plan-value">$99</div>
-          <div className="plan-duration">per month</div>
-          <div className="plan-action center">Choose plan</div>
-        </div>
-      </div>
-      <div className="pricing-table">
-        <div className="pricing-row">
-          <div className="pricing-name">create profile upload for free</div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
+        <div className="plans-wrapper premium-plans">
+          <div className="plan-name">
+            Premium
+            <div className="premium-gift">Gift Subscription</div>
           </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
+          <div className="plan-value">$17 per / month</div>
+          <div className="save-plan">
+            Annual (save 29.2%) US$ 17/month <span>US$ 288</span>  US$ 204/year
           </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
+          <div className="choose-btn premium-btn">Choose plan</div>
+          <div className="include">WHAT'S INCLUDED</div>
+          <div className="included-things">
+            <div>1. Create a profile </div>
+            <div>2. Verified profile</div>
+            <div>3. portfolio</div>
+            <div>4. 20 photos</div>
+            <div>5. 6 videos</div>
+            <div>6. Bio</div>
+            <div>7. CV</div>
+            <div>8. Get Invited to a Job</div>
+            <div>9. Apply to unlimited jobs</div>
+            <div>10. Chat with thebrands (clients)directly</div>
           </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Verification</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Verification</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Photos</div>
-          <div className="pricing-img-wrapper">3 Photos</div>
-          <div className="pricing-img-wrapper">6 Photos</div>
-          <div className="pricing-img-wrapper">Unlimited</div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Cv(copy paste box)</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">ReviewsGet invited to job button</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">
-            Showcase thier social media profile
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Notification</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-        </div>
-        <div className="pricing-row">
-          <div className="pricing-name">Rates ( Fixed/Negotiable)</div>
-          <div className="pricing-img-wrapper">
-            <img src={greyCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={filledCircle}></img>
-          </div>
+          <div className="learn-btn">Learn More</div>
         </div>
       </div>
 
