328244e06a2e2029fe5f88519719cbd29ed0a4b5
Sooraj <sooraj.srishti@gmail.com>
2024-05-15 16:28:30 +0530

    issues

--- src/views/TalentDashBoard.js
+++ src/views/TalentDashBoard.js
@@ -250,184 +250,193 @@ const TalentDashBoard = () => {
           </div>
         </div>
 
-        <div className="container-fluid my-4">
-          <div className="row  talent-dashboard-main">
-            <div className="talent-column-one col-lg-7">
-              <div className="recent-gigs-title">Most Recent Gigs</div>
-              {gigsList.length && (
-                <div className="recent-gigs-main">
-                  {gigsList.map((item, index) => {
-                    return (
-                      <>
-                        <div className="recent-gigs-wrapper">
-                          <div className="recent-setone">
-                            <div className="recent-img-div">
-                              {/* <i className="bi bi-briefcase-fill "></i> */}
-                              <img
-                                className="recent-img"
-                                src={`${API.userFilePath}${item?.brandImage}`}
-                                alt=""
-                              />
-                            </div>
-                            <div className="recent-gig-details">
-                              <div className="recent-gig-company">
-                                {item.hiringCompany}
-                              </div>
-                              <div className="recent-gig-name">
-                                {item.jobTitle}
-                              </div>
-                              <div className="recent-gig-description">
-                                {item.description}
-                              </div>
-                            </div>
-                          </div>
-                          <div className="recent-settwo">
-                            <div className="recent-gigs-count-wrapper">
-                              <div className="recent-gigs-logo">
-                                <i className="bi bi-person-check-fill"></i>
-                              </div>
-                              <div className="recent-gig-count-details">
-                                <div className="recent-gig-name">Followers</div>
-                                <div className="recent-gigs-count">2500</div>
-                              </div>
-                            </div>
-                            <div className="recent-gigs-count-wrapper">
-                              <div className="recent-gigs-logo">
-                                <i className="bi bi-person-arms-up"></i>
-                              </div>
-                              <div className="recent-gig-count-details">
-                                <div className="recent-gig-name">Age</div>
-                                <div className="recent-gigs-count">
-                                  {item.age}
-                                </div>
-                              </div>
-                            </div>
-                            <div className="recent-gigs-count-wrapper">
-                              <div className="recent-gigs-logo">
-                                <i className="bi bi-gender-ambiguous"></i>
-                              </div>
-                              <div className="recent-gig-count-details">
-                                <div className="recent-gig-name">Gender</div>
-                                <div className="recent-gigs-count">
-                                  {item.gender}
-                                </div>
-                              </div>
-                            </div>
-                            <div className="recent-gigs-count-wrapper">
-                              <div className="recent-gigs-logo">
-                                <i className="bi bi-geo-alt-fill"></i>
-                              </div>
-                              <div className="recent-gig-count-details">
-                                <div className="recent-gig-name">Location</div>
-                                <div className="recent-gigs-count">
-                                  {item.jobLocation}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="recent-set-three">
-                            <div
-                              className="view-gig-btn"
-                              onClick={() => {
-                                viewJob(item?._id);
-                              }}
-                            >
-                              <i className="bi bi-eye-fill"></i>
-                              <div>View Job</div>
-                            </div>
-                            <div
-                              className={
-                                item?.isApplied
-                                  ? " apply-now-btn"
-                                  : "apply-now-btn applied-btn"
-                              }
-                              style={{
-                                backgroundColor:
-                                  item?.isApplied == "Apply Now"
-                                    ? "yellow"
-                                    : "green",
-                                color:
-                                  item?.isApplied == "Apply Now"
-                                    ? "black"
-                                    : "#FFFFFF",
-                              }}
-                              onClick={() => {
-                                applyjobs(item);
-                              }}
-                            >
-                              {item?.isApplied == "Applied" && (
-                                <>
-                                  <i className="bi bi-check-circle-fill"></i>
-                                </>
-                              )}
-                              {item?.isApplied == "Apply Now" && (
-                                <>
-                                  <i className="bi bi-briefcase-fill"></i>
-                                </>
-                              )}
+        <div className="container-fluid my-2">
+          <div className="row talent-dashboard-main">
 
-                              <div>{item?.isApplied}</div>
-                            </div>
-                          </div>
-                        </div>
-                      </>
-                    );
-                  })}
-                </div>
-              )}
-              {gigsList.length == 0 && (
-                <div className="recent-gigs-main">No Jobs Available</div>
-              )}
-            </div>
-
-            <div className="talent-column-two col-lg-4">
-              <div className="contact-section-main">
-                <div className="contact-wrapper">
-                  <div className="contact-logo">
-                    <img src={headsetLogo} alt="" />
-                  </div>
-                  <p className="contact-q">Seeking Assistance?</p>
-                  <div className="contact-description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Corrupti, voluptatum labore aspernatur at temporibus
-                  </div>
-                  <div className="contact-btn">Contact Now</div>
-                </div>
-                <div className="top-brands-section">
-                  <div className="top-brands-title">Top Brands</div>
-                  <div className="view-all-brands">View All</div>
-                </div>
-                {topBrandsList.length && (
-                  <div className="top-brands-main">
-                    {topBrandsList.map((item, index) => {
-                      return (
-                        <>
-                          <div className="top-brands-wrapper">
-                            <div className="top-brand-img-wrapper">
-                              {item?.brandImage?.length > 0 ? (
-                                <img
-                                  className="top-brand-img"
-                                  src={
-                                    API.userFilePath +
-                                    item?.brandImage[0].fileData
+            <div className="col-lg-8">
+              <div className="talent-column-one">
+                  <div className="recent-gigs-title">Most Recent Gigs</div>
+                  {gigsList.length && (
+                    <div className="recent-gigs-main">
+                      {gigsList.map((item, index) => {
+                        return (
+                          <>
+                            <div className="recent-gigs-wrapper">
+                              <div className="recent-setone">
+                                <div className="recent-img-div">
+                                  {/* <i className="bi bi-briefcase-fill "></i> */}
+                                  <img
+                                    className="recent-img"
+                                    src={`${API.userFilePath}${item?.brandImage}`}
+                                    alt=""
+                                  />
+                                </div>
+                                <div className="recent-gig-details">
+                                  <div className="recent-gig-company">
+                                    {item.hiringCompany}
+                                  </div>
+                                  <div className="recent-gig-name">
+                                    {item.jobTitle}
+                                  </div>
+                                  <div className="recent-gig-description">
+                                    {item.description}
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="recent-settwo">
+                                <div className="recent-gigs-count-wrapper">
+                                  <div className="recent-gigs-logo">
+                                    <i className="bi bi-person-check-fill"></i>
+                                  </div>
+                                  <div className="recent-gig-count-details">
+                                    <div className="recent-gig-name">Followers</div>
+                                    <div className="recent-gigs-count">2500</div>
+                                  </div>
+                                </div>
+                                <div className="recent-gigs-count-wrapper">
+                                  <div className="recent-gigs-logo">
+                                    <i className="bi bi-person-arms-up"></i>
+                                  </div>
+                                  <div className="recent-gig-count-details">
+                                    <div className="recent-gig-name">Age</div>
+                                    <div className="recent-gigs-count">
+                                      {item.age}
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="recent-gigs-count-wrapper">
+                                  <div className="recent-gigs-logo">
+                                    <i className="bi bi-gender-ambiguous"></i>
+                                  </div>
+                                  <div className="recent-gig-count-details">
+                                    <div className="recent-gig-name">Gender</div>
+                                    <div className="recent-gigs-count">
+                                      {item.gender}
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="recent-gigs-count-wrapper">
+                                  <div className="recent-gigs-logo">
+                                    <i className="bi bi-geo-alt-fill"></i>
+                                  </div>
+                                  <div className="recent-gig-count-details">
+                                    <div className="recent-gig-name">Location</div>
+                                    <div className="recent-gigs-count">
+                                      {item.jobLocation}
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="recent-set-three">
+                                <div
+                                  className="view-gig-btn"
+                                  onClick={() => {
+                                    viewJob(item?._id);
+                                  }}
+                                >
+                                  <i className="bi bi-eye-fill"></i>
+                                  <div>View Job</div>
+                                </div>
+                                <div
+                                  className={
+                                    item?.isApplied
+                                      ? " apply-now-btn"
+                                      : "apply-now-btn applied-btn"
                                   }
-                                  alt=""
-                                />
-                              ) : (
-                                <div>No Image Available</div>
-                              )}
+                                  style={{
+                                    backgroundColor:
+                                      item?.isApplied == "Apply Now"
+                                        ? "yellow"
+                                        : "green",
+                                    color:
+                                      item?.isApplied == "Apply Now"
+                                        ? "black"
+                                        : "#FFFFFF",
+                                  }}
+                                  onClick={() => {
+                                    applyjobs(item);
+                                  }}
+                                >
+                                  {item?.isApplied == "Applied" && (
+                                    <>
+                                      <i className="bi bi-check-circle-fill"></i>
+                                    </>
+                                  )}
+                                  {item?.isApplied == "Apply Now" && (
+                                    <>
+                                      <i className="bi bi-briefcase-fill"></i>
+                                    </>
+                                  )}
+
+                                  <div>{item?.isApplied}</div>
+                                </div>
+                              </div>
                             </div>
-                            <div className="top-brands-name">
-                              {item.brandName}
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                )}
+                          </>
+                        );
+                      })}
+                    </div>
+                  )}
+                  {gigsList.length == 0 && (
+                    <div className="recent-gigs-main">No Jobs Available</div>
+                  )}
               </div>
             </div>
+            <div className="col-lg-4">
+              <div className="rightBx">
+                <div className="contact-section-main remvSpace">
+                  <div className="contact-wrapper boxsWhite mb-4">
+                    <div className="contact-logo">
+                      <img src={headsetLogo} alt="" />
+                    </div>
+                    <p className="contact-q">Seeking Assistance?</p>
+                    <div className="contact-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Corrupti, voluptatum labore aspernatur at temporibus
+                    </div>
+                    <div className="contact-btn">Contact Now</div>
+                  </div>
+
+                  <div className="boxsWhite mb-4">
+                    <div className="top-brands-section">
+                      <div className="top-brands-title">Top Brands</div>
+                      <div className="view-all-brands">View All</div>
+                    </div>
+                    {topBrandsList.length && (
+                      <div className="top-brands-main">
+                        {topBrandsList.map((item, index) => {
+                          return (
+                            <>
+                              <div className="top-brands-wrapper">
+                                <div className="top-brand-img-wrapper">
+                                  {item?.brandImage?.length > 0 ? (
+                                    <img
+                                      className="top-brand-img"
+                                      src={
+                                        API.userFilePath +
+                                        item?.brandImage[0].fileData
+                                      }
+                                      alt=""
+                                    />
+                                  ) : (
+                                    <div>No Image Available</div>
+                                  )}
+                                </div>
+                                <div className="top-brands-name">
+                                  {item.brandName}
+                                </div>
+                              </div>
+                            </>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </main>
