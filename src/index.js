2dad7da7129385282b00d7aba65b3a8ec45902c2
ratheesh-srishtis <113422448+ratheesh-srishtis@users.noreply.github.com>
2024-08-07 17:44:42 +0530

    code-change

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { StyledEngineProvider } from "@mui/material";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-
-const App = lazy(() => import("./App"));
+// basename=""
+// "homepage": "https://brandsandtalent.com/",
 
 ReactDOM.render(
   <BrowserRouter>
     <GoogleOAuthProvider clientId="301564582988-fei7hvri7467l3jkru3ggn7f7tvohg0j.apps.googleusercontent.com">
       <StyledEngineProvider injectFirst>
-        <Suspense fallback={<div>Loading...</div>}>
-          <App />
-        </Suspense>
+        <App />
       </StyledEngineProvider>
     </GoogleOAuthProvider>
   </BrowserRouter>,
